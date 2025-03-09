9962c30e0793a4f2fac10b19b726dacf3a19a39c
clzll <ksalen6school@gmail.com>
2025-03-09 10:32:34 +0300

    Fix ci-extra/check-asm-formatting.py

--- ci-extra/check-asm-formatting.py
+++ ci-extra/check-asm-formatting.py
@@ -1,9 +1,11 @@
 #!/usr/bin/env python3
 
 import glob
+import itertools
 
 def check(filename: str):
     tabs = set()
+    prev_no_backslash = False
 
     with open(filename) as file:
         for line_no, line in enumerate(file, 1):
@@ -16,9 +18,12 @@ def check(filename: str):
 
             first_non_ws_pos = len(right_stripped) - len(right_stripped.lstrip())
             if first_non_ws_pos == 0:
+                prev_no_backslash = True
                 continue
 
-            tabs.add(line[:first_non_ws_pos])
+            if prev_no_backslash:
+                tabs.add(line[:first_non_ws_pos])
+            prev_no_backslash = right_stripped[-1] != '\\'
 
     if len(tabs) > 1:
         print(f'Too many unique tabulation sequences in {filename}:')
@@ -26,5 +31,5 @@ def check(filename: str):
             print(f'\t- {repr(tab)}')
         exit(1)
 
-for filename in glob.iglob('src/*.asm'):
+for filename in itertools.chain(glob.iglob('src/*.asm'), glob.iglob('include/*.inc')):
     check(filename)
